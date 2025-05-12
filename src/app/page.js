55e5a3822cebefeb2766776c032ca9b51ceb353d
Its-Abhinav-Jangid <zangidnehul2020@gmail.com>
2025-05-12 16:44:33 +0530

    [Bug Fix] expense data fetching end date set to tommorow from today to resolve time zone issues

--- src/app/page.js
+++ src/app/page.js
@@ -12,15 +12,16 @@ async function fetchExpenseData() {
   const baseURL = process.env.API_BASE_URL;
   const sessionCookie = await getCookie(process.env.AUTH_COOKIE_NAME); // required for api authentication
 
-  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
   const prevMonth = new Date();
 
-  prevMonth.setMonth(today.getMonth() - 1);
+  prevMonth.setMonth(new Date().getMonth() - 1);
 
   const {
     data: [prev30DaysExpensesSummary],
   } = await axios.get(
-    `${baseURL}/api/expenses/summary?startDate=${prevMonth}&endDate=${today}&includeDailyData=true`,
+    `${baseURL}/api/expenses/summary?startDate=${prevMonth}&endDate=${tomorrow}&includeDailyData=true`,
     {
       headers: {
         Cookie: `${sessionCookie.name}=${sessionCookie.value}`, // Important for authentication
@@ -29,7 +30,7 @@ async function fetchExpenseData() {
   );
 
   const { data: prev30DaysExpenses } = await axios.get(
-    `${baseURL}/api/expenses?startDate=${prevMonth}&endDate=${today}`,
+    `${baseURL}/api/expenses?startDate=${prevMonth}&endDate=${tomorrow}`,
     {
       headers: {
         Cookie: `${sessionCookie.name}=${sessionCookie.value}`,
