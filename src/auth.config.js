79846a1e2d142105554b7556bd9e4fe6b8daaac9
Its-Abhinav-Jangid <zangidnehul2020@gmail.com>
2025-06-01 23:11:54 +0530

    Google login added

--- src/auth.config.js
+++ src/auth.config.js
@@ -1,6 +1,6 @@
 import GitHub from "next-auth/providers/github";
-
+import Google from "next-auth/providers/google";
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
+  providers: [Google, GitHub],
 };
