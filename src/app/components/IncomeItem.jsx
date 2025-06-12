4e60a760de6cad60d7515b2f0932e7a5aaa42f11
Its-Abhinav-Jangid <zangidnehul2020@gmail.com>
2025-06-12 19:47:31 +0530

    recurring tag added in incomeItem

--- src/app/components/IncomeItem.jsx
+++ src/app/components/IncomeItem.jsx
@@ -1,9 +1,16 @@
 import { categoryColors, categoryIcons } from "../lib/constants/categoryIcons";
 import { HiOutlineCalendar } from "react-icons/hi";
 import { formatAmount } from "../lib/formatAmount";
-function IncomeItem({ id, category, date, amount }) {
+function IncomeItem({ id, category, date, amount, isRecurring }) {
   return (
-    <div className="bg-gray-800 rounded-xl p-4 hover:bg-gray-750 transition-colors">
+    <div className="relative bg-gray-800 rounded-xl p-4 hover:bg-gray-750 transition-colors">
+      {isRecurring && (
+        <div className="absolute top-1 right-2">
+          <div className="flex items-center text-xs bg-gray-700 text-gray-300 px-2 py-1 rounded-full transition-colors">
+            Recurring
+          </div>
+        </div>
+      )}
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-3">
           <div
