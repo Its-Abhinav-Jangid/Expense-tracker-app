8741198cd913f970ddc3579cc7d650d32b2eaaa6
Abhinav-Jangid <zangidnehul2020@gmail.com>
2025-06-21 18:18:57 +0530

    Fix: provider parameter added in login

--- src/app/lib/actions/auth.js
+++ src/app/lib/actions/auth.js
@@ -2,8 +2,8 @@
 
 import { signIn, signOut } from "@/auth";
 
-export const login = async () => {
-  await signIn("github", { redirectTo: "/app" });
+export const login = async (provider) => {
+  await signIn(provider, { redirectTo: "/app" });
 };
 export const logout = async () => {
   await signOut({ redirectTo: "/signin" });
